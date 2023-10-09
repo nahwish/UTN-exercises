9ffa564dfd0590907a697ec34ddb21379e484036
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-10-09 20:31:37 -0300

    working class05

--- Class05/Exercise03C05.cs
+++ Class05/Exercise03C05.cs
@@ -6,18 +6,6 @@ descendente o no tiene orden.
 */
 class Exercise03C05
 {
-  public static void VerifyArrayNumbers()
-  {
-    Console.WriteLine("escribir numeros separados por coma: ");
-    string numbers = Console.ReadLine();
-    string[] arrStr = numbers.Split(',');
-    string[] copia = new String[5];
-    copia = arrStr;
-    Array.Sort(arrStr);
-    if(arrStr == copia)
-    {
-      Console.WriteLine("entró");
-    }
-  }
+
 
 }
